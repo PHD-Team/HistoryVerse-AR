89dcc3fb6764499ce514114493b4977b8cc6ad17
kareem <kareemesam52@yahoo.com>
2024-04-25 15:32:24 +0200

    add FireBaseAnchor model

--- app/src/main/java/com/magic/ui/localization/models/FireBaseAnchor.kt
+++ app/src/main/java/com/magic/ui/localization/models/FireBaseAnchor.kt
@@ -1,10 +1,11 @@
 package com.magic.ui.localization.models
 
 data class FireBaseAnchor(
-    val anchor: String = "",
-    val order: Int = 1,
+    val anchor : String = "" ,
+    val order : Int = 1 ,
 )
+
 data class FireBasePath(
-    val order: Int = 1,
-    val anchors: List<FireBaseAnchor> = emptyList()
+    val order : Int = 1 ,
+    val anchor : List<FireBaseAnchor>? = emptyList() ,
 )
